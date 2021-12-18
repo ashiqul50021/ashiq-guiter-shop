3e2fbb10de24c6643bbc42a16daf0c43afe60c1b
JannatulHappy <jannatulferdoushappy21@gmail.com>
2021-12-18 23:45:59 +0600

    second-commit

--- src/Pages/Firebase/firebase.config.js
+++ src/Pages/Firebase/firebase.config.js
@@ -7,16 +7,31 @@
 //     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 //     appId: process.env.REACT_APP_FIREBASE_APP_ID,
 //   };
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC89vdeZvKZCChlqyWM7Y3mEtnj20CcG-Q",
+//   authDomain: "acoustica-e28a1.firebaseapp.com",
+//   projectId: "acoustica-e28a1",
+//   storageBucket: "acoustica-e28a1.appspot.com",
+//   messagingSenderId: "486291334893",
+//   appId: "1:486291334893:web:a687afe7a558adea744ee4"
+// };
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCORZF3EGq0BI6mopT-uevFI1Gr_YTfsNA",
-  authDomain: "acoustica-97295.firebaseapp.com",
-  projectId: "acoustica-97295",
-  storageBucket: "acoustica-97295.appspot.com",
-  messagingSenderId: "960767955560",
-  appId: "1:960767955560:web:0dc6f287e2bd1df1cda1dd"
+  apiKey: "AIzaSyCT_9-3adA4axxAgbsfnDvHJGar2SZOe_8",
+  authDomain: "acoustica-78f5e.firebaseapp.com",
+  projectId: "acoustica-78f5e",
+  storageBucket: "acoustica-78f5e.appspot.com",
+  messagingSenderId: "337047920489",
+  appId: "1:337047920489:web:29ec1534267a34ae2527f8"
 };
+
 
   export default firebaseConfig;
   
-  
+//   // REACT_APP_FIREBASE_API_KEY=AIzaSyCORZF3EGq0BI6mopT-uevFI1Gr_YTfsNA
+// REACT_APP_FIREBASE_AUTH_DOMAIN=acoustica-97295.firebaseapp.com
+// REACT_APP_FIREBASE_PROJECT_ID=acoustica-97295
+// REACT_APP_FIREBASE_STORAGE_BUCKET=acoustica-97295.appspot.com
+// REACT_APP_FIREBASE_MESSAGING_SENDER_ID=960767955560"
+// REACT_APP_FIREBASE_APP_ID=1:960767955560:web:0dc6f287e2bd1df1cda1dd
